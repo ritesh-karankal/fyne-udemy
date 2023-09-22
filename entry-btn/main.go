be544d50c1597f186d675f7b084a2db98e0915d5
Ritesh Karankal <ritesh.karankal200@gmail.com>
2023-09-22 11:05:57 +0530

    update

--- entry-btn/main.go
+++ entry-btn/main.go
@@ -7,18 +7,24 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+type App struct {
+	output *widget.Label
+}
+
+var MyApp App 
+
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello!")
 
-	output, entry, btn := makeUI()
+	output, entry, btn := MyApp.makeUI()
 	
 	w.SetContent(container.NewVBox(output, entry, btn))
 	w.Resize(fyne.NewSize(500, 500))
 	w.ShowAndRun()
 }
 
-func makeUI() (*widget.Label, *widget.Entry, *widget.Button) {
+func (app *App) makeUI() (*widget.Label, *widget.Entry, *widget.Button) {
 	output := widget.NewLabel("Hello World!")
 	entry := widget.NewEntry()
 	btn := widget.NewButton("Enter", func() {
@@ -26,5 +32,7 @@ func makeUI() (*widget.Label, *widget.Entry, *widget.Button) {
 	})
 	btn.Importance = widget.HighImportance
 	
+	app.output = output
+
 	return output, entry, btn
 }
